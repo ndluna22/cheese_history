a082bb1e5d1bddfe4c436b0577c1b99efc192f60
Nadia Luna <ndaluna@gmail.com>
2023-08-20 22:59:50 -0500

    classes

---
+++ src/Joke.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Joke.css";
+
+// function Joke({ vote, votes, text, id }) {
+//   const upVote = () => vote(id, +1);
+//   const downVote = () => vote(id, -1);
+
+class Joke extends Component {
+  constructor(props) {
+    super(props);
+    this.upVote = this.upVote.bind(this);
+    this.downVote = this.downVote.bind(this);
+  }
+
+  upVote() {
+    this.props.vote(this.props.id, +1);
+  }
+
+  downVote() {
+    this.props.vote(this.props.id, -1);
+  }
+
+  render() {
+    return (
+      <div className="Joke">
+        <div className="Joke-votearea">
+          <button onClick={upVote}>
+            <i className="fas fa-thumbs-up" />
+          </button>
+
+          <button onClick={downVote}>
+            <i className="fas fa-thumbs-down" />
+          </button>
+
+          {votes}
+        </div>
+
+        <div className="Joke-text">{text}</div>
+      </div>
+    );
+  }
+}
+
+export default Joke;
